57b3dfcf506b6e799d71666b8120b8dd2ccdf86f
Iñigo Solano <i@errstate.com>
2021-11-13 21:34:14 +0100

    add personal emacs config

--- emacs-profiles.el
+++ emacs-profiles.el
@@ -1,4 +1,3 @@
-(("self" . ((user-emacs-directory . "~/.emacses/self/emacs.d")))
- ("self2" . ((user-emacs-directory . "~/.emacses/self2/emacs.d")))
+(("self" . ((user-emacs-directory . "~/.emacs-config/emacs.d")))
  ("spacemacs" . ((user-emacs-directory . "~/.emacses/spacemacs/emacs.d")))
  ("doom" . ((user-emacs-directory . "~/.emacses/doom/doom-emacs"))))
